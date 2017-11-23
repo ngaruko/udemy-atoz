c12198175109344c83543aff8c9566c91feed764
ngaruko <ngarukob@yahoo.com>
2017-11-24 12:47:02 +1300

    Multiple linear regression

--- fundamentals/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ fundamentals/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -7,24 +7,32 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
+#X: all columns but the last one which is the dependent variable
 X = dataset.iloc[:, :-1].values
+#last column == dependent variable
 y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
+'''Create dummy variable 0 and 1 for states: California, Florida, New York'''
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#LabelEncoder :: wighed index
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
+#OneHotEncoder ::
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
+'''Take all comuns from index one. Remove one columns...
+this is normally taken care by the pythobn lib used >>
+to avoid redundant dependencies'''
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling >> No need the library will take care of this
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -38,4 +46,6 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
+'''To be compared with our real profit y-test 
+to measure how good our model is'''
 y_pred = regressor.predict(X_test)
