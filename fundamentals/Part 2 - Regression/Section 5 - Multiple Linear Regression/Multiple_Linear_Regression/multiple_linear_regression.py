905a6338d0b687c810103392819da145694d8abf
ngaruko <ngarukob@yahoo.com>
2017-11-24 13:59:16 +1300

    Building the optimal model using Backward Elimination in Python

--- fundamentals/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ fundamentals/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -49,3 +49,49 @@ regressor.fit(X_train, y_train)
 '''To be compared with our real profit y-test 
 to measure how good our model is'''
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+'''Some variables might be more statistically significant than others'''
+import statsmodels.formula.api as sm
+#needed to add X0=1 for constant b0 for the library to include the constant
+'''use append funtion of Numpy'''
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X[:,[0, 1, 2, 3, 4, 5]]
+
+#OLS = Ordinary least squares
+regressor_OLS =  sm.OLS(endog = y, exog = X_opt).fit()
+
+#check P values. 
+'''Backward Elimination...
+Step 1: Select a significance level to stay in the model (eg SL= 0.05)
+Step 2. Fit the full model with all possible predictors
+Step 3: Consider the predictor with the highest P-value. If P > SL, remove the predictor.
+otherwise your model is ready
+Step 4. Fit the model without this variable'''
+regressor_OLS.summary()
+
+# X2 with highest P value
+X_opt = X[:,[0, 1, 3, 4, 5]]
+regressor_OLS =  sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+# X1 with highest P value 
+X_opt = X[:,[0, 3, 4, 5]]
+regressor_OLS =  sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# X4 with highest P value
+X_opt = X[:,[0, 3, 5]]
+regressor_OLS =  sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+#marketing spend 0.06 a bit higher than 0.05 chosen
+# X4 with highest P value
+X_opt = X[:,[0, 3]]
+regressor_OLS =  sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#RD-spend is the powerful predictor
+# strongest impact is the RD spend according to our model
