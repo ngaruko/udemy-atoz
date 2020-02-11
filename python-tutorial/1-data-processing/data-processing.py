04615a132f44c3882aa1e7a56f920ca3ba436b3c
ngaruko <bede.ngaruko@gmail.com>
2020-02-11 19:21:05 +1300

    missing data

--- python-tutorial/1-data-processing/data-processing.py
+++ python-tutorial/1-data-processing/data-processing.py
@@ -2,7 +2,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#for missing data
+from sklearn.impute import SimpleImputer
+
 dataset=pd.read_csv('./res/data.csv')
-z = dataset.iloc[:, :-1]
 X= pd.DataFrame(dataset.iloc[:, :-1].values)
 y = pd.DataFrame(dataset.iloc[:, 3].values)
+
+#missing data
+
+
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean',verbose=0)
+
+imputer = imputer.fit(X.iloc[:, 1:3])
+
+X.iloc[:, 1:3] = imputer.transform(X.iloc[:, 1:3])
