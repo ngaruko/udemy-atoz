1686d55875217f8a3c9d81d6efb1eebaa2c87c37
ngaruko <bede.ngaruko@gmail.com>
2020-02-11 18:44:43 +1300

    import-data

--- python-tutorial/1-data-processing/data-processing.py
+++ python-tutorial/1-data-processing/data-processing.py
@@ -2,3 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+dataset=pd.read_csv('./res/data.csv')
+z = dataset.iloc[:, :-1]
+X= pd.DataFrame(dataset.iloc[:, :-1].values)
+y = pd.DataFrame(dataset.iloc[:, 3].values)
